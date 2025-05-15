b8ef929265d0279c2217fac2f705af002b7ac6a2
chandra <chandraannem01@gmail.com>
2025-05-15 12:39:25 +0530

    4th

--- hello.sh
+++ hello.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-echo "Hello, World!"
-echo "This is a simple shell script."
 
-# This script demonstrates how to use variables and loops in bash
-# Define a variable
-greeting="Hello, User!"
-# Print the greeting
-echo $greeting
 # Loop through numbers 1 to 5
 for i in {1..5}
 do
