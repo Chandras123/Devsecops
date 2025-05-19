d3459422b16913406eefcca27d681e47ccbc6e71
chandra <chandraannem01@gmail.com>
2025-05-19 21:51:30 +0530

    hello start

---
+++ shell.sh/conv.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#writing script bettween two peoples
+echo "hello welcome mok"
+echo "yeah hai chand"
+echo "hello chand"
+echo "hhello mok"
+echo "this is starting of shell language chand"
