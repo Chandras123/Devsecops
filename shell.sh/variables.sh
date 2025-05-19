6e78d2e81e168eba1a8a9c44e8400464854a28cf
chandra <chandraannem01@gmail.com>
2025-05-19 22:14:17 +0530

    hello start

--- shell.sh/variables.sh
+++ shell.sh/variables.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-#writing script bettween two peoples
-p1=$1
-p2=$2
-echo "hello welcome $p1"
-echo "yeah hai $p2"
-echo "hello $p2"
-echo "hhello $p1"
-echo "this is starting of shell language $p2"
+person1="$person1"
+person2="$person2"
+echo "hello welcome $person1"
+echo "yeah hai $person2"
+echo "hello $person2"
+echo "hhello $person1"
+echo "this is starting of shell language $person2"
