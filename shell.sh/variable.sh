df4a350b2c2a31f46b430ff9dfcd4925a0a1989c
chandra <chandraannem01@gmail.com>
2025-05-19 21:21:07 +0530

    hello start

---
+++ shell.sh/variable.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# This script demonstrates the use of variables in bash
+# Variables
+# Variables are used to store data that can be used later in the script
+# Variable names can contain letters, numbers, and underscores
+
+echo "Hello, World!" # Print a message to the console
