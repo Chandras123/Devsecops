7f1d89d2b7ba7c900a51a021ae52b3f9995618bc
chandra <chandraannem01@gmail.com>
2025-05-19 22:18:28 +0530

    hello start

--- shell.sh/variables.sh
+++ shell.sh/variables.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-person1=mok
-person2=chand
-echo "hello welcome $person1"
-echo "yeah hai $person2"
-echo "hello $person2"
-echo "hhello $person1"
-echo "this is starting of shell language $person2"
+# This script demonstrates the use of variables in bash
+# Variables
+person1= mok
+person2= chand
+echo " $person2 hello welcome $person1 "
+echo " yeah hai $person2 "
+echo " hello $person2 "
+echo " hhello $person1 "
+echo " this is starting of shell language $person2 "
